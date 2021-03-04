48a30b83829dc31138ec2b8d62703b76496452e2
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-03-04 14:06:05 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// connect to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// loggin middleware
+app.use(logger("dev"));
+
+// post request middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+// static folder
+app.use(express.static("public"));
+
+// routes
+
+
+app.listen(PORT, () => {
+    console.log(`You're running on port ${PORT}`);
+})
